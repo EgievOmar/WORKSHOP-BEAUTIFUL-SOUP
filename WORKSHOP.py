58a96911c01db03656c07cd342e84850c6ec2e51
EgievOmar <omar.egiev@epitech.eu>
2023-02-07 17:35:22 +0100

    exo 1

--- WORKSHOP.py
+++ WORKSHOP.py
@@ -13,6 +13,7 @@ def beautiful_requests(url):
 # Affiche le lien de l'image de l'URL donnée
 def exercise_1(url_exercise1):
     response = beautiful_requests(url_exercise1)
+    print(url_exercise1)
 
 
 # Exercise 2
@@ -28,10 +29,10 @@ def exercise_3(url_exercise3):
 
 
 if __name__ == '__main__':
-    ton_pseudo_github = 'ton_pseudo_github'
+    ton_pseudo_github = 'EgievOmar'
     url_exercise1 = 'https://github.com/' + ton_pseudo_github
     url_exercise2 = 'https://www.imdb.com/chart/top/'
     url_exercise3 = 'https://fr.wikipedia.org/wiki/Liste_des_pays_par_PIB_nominal'
-    # exercise_1(url_exercise1)
-    # exercise_2(url_exercise2)
-    # exercise_3(url_exercise3)
+    exercise_1(url_exercise1)
+    exercise_2(url_exercise2)
+    exercise_3(url_exercise3)
